593d584cdedfa1ae900267d74bd0363cb037bfc9
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-13 21:52:33 +0100

    Changes Commited

--- weather_app/main_app/forms.py
+++ weather_app/main_app/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class TownForm(forms.Form):
-    town_name = forms.CharField(label='Town Name', max_length=100,)
+    town_name = forms.CharField(label='Enter Town Name', max_length=100,)
