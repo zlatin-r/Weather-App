a30577e36fd2629b8befe20e58d67c00ef8f698d
Zlatin Rusev <zlatinrusev@gmail.com>
2024-11-13 21:34:00 +0100

    Changes Commited

--- weather_app/main_app/views.py
+++ weather_app/main_app/views.py
@@ -13,7 +13,7 @@ def index(request):
             town_name = form.cleaned_data['town_name']
 
             # Replace 'your_api_key' with your actual API key
-            api_key = 'vo36ezJu0PwEZHNxGBr2H8LLAKjyyF18'
+            api_key = '826b97ae066d39e279901e86a67f6419'
             url = f'https://api.openweathermap.org/data/2.5/weather?q={town_name}&appid={api_key}&units=metric'
 
             # Send request to the weather API
